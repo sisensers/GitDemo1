ad4200f4444fe27c52abf88c1a82e84ec31fce10
Sisense POC <admin%40sisensepoc.com>
2022-11-18 14:50:53 +0000

    initial commit

---
+++ dashboards/63779a6c77532a0038372d8b/widgets/63779a6c77532a0038372d90/script.js
@@ -0,0 +1,21 @@
+widget.on('ready', function(se, ev){
+
+Accordion({
+
+element: element,
+
+widget: widget,
+
+dashboard: dashboard,
+
+//filters: ['[<table name>.<table field>]'],
+
+default: false,
+
+dashboardUrl: '/app/main#/dashboards/5e70c0971092ad1c780e6690',
+
+widgetFiltersInheritance: true 
+
+});
+
+})
---
+++ dashboards/63779a6c77532a0038372d8b/widgets/63779a6c77532a0038372d94/script.js
@@ -0,0 +1,17 @@
+/*
+Welcome to your Widget's Script.
+
+To learn how you can access the Widget and Dashboard objects, see the online documentation at http://developer.sisense.com/pages/viewpage.action?pageId=557127
+*/
+
+widget.on('ready', function(se, ev){
+
+setTimeout(function(){
+
+ $('.p-value a',element)
+
+ .css({'color': 'white' , 'text-decoration':'none'}) // change styling
+
+    },500);
+
+})
